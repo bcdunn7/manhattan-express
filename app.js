02676b553a103e2368ae7a7e79ce4421516dec99
Bryce Dunn <bryce.cc.dunn@gmail.com>
2023-06-15 16:22:07 -0500

    feat: set up basic chatGPT request

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const { Configuration, OpenAIApi } = require('openai');
 const cors = require('cors');
+
+
 const app = express();
 const port = 3001;
+
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,7 +18,6 @@ const openai = new OpenAIApi(configuration);
 app.use(cors({
   origin: ['http://localhost:5173'],
 }));
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('All aboard the Manhattan Express! This is a POC for Egen GPTJam. Team: Project Manhattan.');
@@ -23,6 +27,22 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.post('/', (req, res) => {
-  res.json({resp: "Got a POST request"});
+app.post('/', async (req, res) => {
+  try {
+    const chatCompletion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo-0613',
+      messages: [{role: 'user', content: 'Hello world'}],
+    })
+
+    res.status(200).json({resp: chatCompletion.data.choices[0]});
+  } catch (error) {
+    if (error.response) {
+      console.warn('ERROR');
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json({error: error.response.data});
+    } else {
+      console.error(`ERROR with OpenAI API request: ${error.message}`);
+      res.status(500).json({error: {message: 'An error occured'}})
+    }
+  }
 });
